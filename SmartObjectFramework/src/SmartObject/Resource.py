817df8b3a84c341ecdf800dd5ce586166f4d6a16
MIchael Koster <michaeljohnkoster@gmail.com>
2012-09-30 06:49:24 -0700

    Refactor sync

--- SmartObjectFramework/src/SmartObject/Resource.py
+++ SmartObjectFramework/src/SmartObject/Resource.py
@@ -22,23 +22,33 @@ class Resource(object) :
     # when this resource is deleted
     def __del__(self):
         pass
+        
+    @property
+    def __get__(self):
+        return self.get
     
+    @property
+    def __set__(self, newValue):
+        self.set(newValue)
+        return
+
+    # return the default contents of this resource
+    @property
+    def get(self) :
+        return(self.value)
+
+    # update the default contents of this resource
+    @property
+    def set(self, newValue) :
+        self.value=newValue
+        return
+      
     # for adding resources inside this resource
     def create(self, resource) :
-        self.resources.add(resource);
+        self.resources.add(resource)
         return
 
     # for removing resources inside this resource
     def delete(self, resource) :
-        self.resources.remove(resource);
+        self.resources.remove(resource)
         return
-    
-    # update the default contents of this resource
-    def set(self, newValue) :
-        self.value=newValue;
-        return
-     
-    # return the default contents of this resource
-    def get(self) :
-        return(self.value);
-
