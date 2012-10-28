f88d36fff2a31462b2d7c0b3fe0e37fc63591b97
MIchael Koster <michaeljohnkoster@gmail.com>
2012-10-28 14:17:07 -0700

    Agent - handler- appHandler code

--- SmartObjectFramework/src/SmartObject/Agent.py
+++ SmartObjectFramework/src/SmartObject/Agent.py
@@ -13,47 +13,82 @@ for debug.
 
 from RESTfulResource import RESTfulResource
 
-class SmartObjectHandler(RESTfulResource):
+# import AppHandlers # import a well known module
+
+
+class AppHandler(object): # template and convenience methods for raw app handlers. Python app handler should extend this class
+    def __init__(self) :
+        self._propertyLinks = {}
+    
+    def _updateHandler(self):
+        pass
+
+
+class RESTfulEndpoint(object):
+    def __init__(self, reference):
+        self._resource = reference
+        
+    def get(self):
+        return self._resource
+    
+    def set(self,newValue):
+        self._resource = newValue
+        return
+    
+    
+class Handler(RESTfulResource):
     
     def __init__(self):
         RESTfulResource.__init__(self)
-        self._propertyLinks = []
-        self._codeModule = None
+        self._propertyLinks = {}
+        self._appHandlerName = None
+        self._updateHandler = None # reference to _updateHandler method of AppHandler
 
-    def codeModule(self):
-        return self._codeModule
+    def appHandlerName(self):
+        return self._appHandlerName
     
     def propertyLinks(self):
         return self._propertyLinks
 
-    def updateMethod(self):
-        return self._updateMethod
-    
-    def _updateMethod(self):
-        pass
-    
+    def updateHandler(self):
+        return self._updateHandler
+        
     def get(self):
-        return self._codeModule
+        return self._appHandlerName
     
-    def set(self,codeModule):
-        self._codeModule = codeModule
+    def set(self,appHandlerName): # create an instance of a code object in this handler object
+        self._appHandlerName = appHandlerName
+        self._moduleName, self._className = self._appHandlerName.split('.')
+        # from <self._moduleName> import <self._className> get the code object into the namespace
+        self._appHandler = globals()[self._className]() # make instance of AppHandler by name
+        # make a resource to read back the AppHandler class name
+        self.resources.update( { 'AppHandler' : RESTfulEndpoint(self._appHandlerName)}) 
+        # set up the property links resources
+        if hasattr( self._appHandler, '_propertyLinks') :
+            self._propertyLinks = self._appHandler._propertyLinks
+            for self._propertyLinkName in self._propertyLinks.keys() : 
+                self.resources.update({self._propertyLinkName : self._propertyLinks[self._propertyLinkName]})
+                
+        # set up the callable property to be invoked on callbacks
+        if hasattr( self.appHandler, '_updateHandler' ) :
+            self._updateHandler = self._appHandler._updateHandler
+               
 
 
 class Agent(RESTfulResource):
     
-    def __init__(self, agent):
+    def __init__(self):
         RESTfulResource.__init__(self)
-        # reference to the code class to create an instance of 
-        # can be passed in on the constructor, or default on init, or be changed later
-        self.defaultClass = 'SmartObjectHandler'
-        self.__handlers = {}
+                
+        self.defaultClass = 'Handler'
+        self._handlers = {}
         
     def get(self, handlerName=None):
         if handlerName == None:
-            return self._handlers.keys()
+            return self._handlers.keys() # to get the list of names
         else:
             if self._handlers.has_key(handlerName) :
-                return self._handlers[handlerName]
+                return self._handlers[handlerName] # to get reference to handler resources by handler name
         return None
     
     '''    
@@ -72,14 +107,9 @@ class Agent(RESTfulResource):
                 className = self.defaultClass 
         # create new instance of the named class and add to resources directory, return the ref
         self.resources.update({resourceName : globals()[className]()}) 
-        if className == self.defaultClass : # Handler class
+        if className == self.defaultClass : # Handler class assumed
             self._handlers.update( {resourceName, self.resources[resourceName]} )
         return self.resources[resourceName] # returns a reference to the created instance
 
- 
-    def delete(self, handlerName):
-        RESTfulResource.delete(handlerName)    
-        # and more...
-        return
         # need to destroy instance of code module
         
